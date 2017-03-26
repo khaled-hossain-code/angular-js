290ead396657ebcb41820079f88535ad0c8e4282
khaled hossain <md.khaled.eee@gmail.com>
2017-03-26 09:47:34 +0600

    template

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
+myApp.config(function($routeProvider){
 
-    $scope.alertClick = function () {
-        // Simple GET request example:
-        $http({
-            method: 'GET',
-            url: 'https://api.datausa.io/api/?show=geo&sumlevel=nation&required=pop'
-        }).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            console.log(response);
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-    }
+    $routeProvider
+    .when('/', {
+        templateUrl: '/pages/main.html',
+        controller: 'mainController'
+    })
+    .when('/second',{
+
+    })
+})
+
+myApp.controller('myController', ['$scope','$location', '$log', function ($scope, $location, $log) {
+
+    $log.info($location.path());
+
 }])
